54677682b960004e2294b1ba0cdf4099f321e3e5
DongHyun Kim <temeddix@gmail.com>
2024-02-16 19:28:18 +0900

    Upgrade `class-transform`

--- index.ts
+++ index.ts
@@ -1,12 +1,11 @@
-import { Exposed, plainToInstance } from "class-transform";
+import { Exposed, plainToInstance, plainsToInstances } from "class-transform";
 
-let photoJson = `
-{
-  "id": 11,
-  "filename": "dirty_dog.jpg",
-  "depth": 2261
-}
-`;
+let photoPlain = {
+  id: 11,
+  filename: "dirty_dog.jpg",
+  depth: 2261,
+};
+let photoJson = JSON.stringify(photoPlain);
 
 class Photo {
   id = Exposed.number();
@@ -17,28 +16,74 @@ class Photo {
 let photo = plainToInstance(Photo, JSON.parse(photoJson));
 console.log(photo);
 
-let albumJson = `
-{
-  "id": 1,
-  "name": "foo",
-  "photos": [
-    {
-      "id": 9,
-      "filename": "cool_whale.jpg",
-      "depth": 1245
+let albumsPlain = [
+  {
+    id: 1,
+    name: "foo",
+    photos: [
+      {
+        id: 9,
+        filename: "cool_whale.jpg",
+        depth: 1245,
+      },
+      {
+        id: 10,
+        filename: "hot_bird.jpg",
+        depth: 6123,
+      },
+    ],
+    pages: {
+      table: 6,
+      contents: 72,
     },
-    {
-      "id": 10,
-      "filename": "hot_bird.jpg",
-      "depth": 6123
-    }
-  ],
-  "pages": {
-    "table": 6,
-    "contents": 72
-  }
-}
-`;
+  },
+  {
+    id: 2,
+    name: "bar",
+    photos: [
+      {
+        id: 11,
+        filename: "awesome_dog.jpg",
+        depth: 500,
+      },
+      {
+        id: 12,
+        filename: "cute_cat.jpg",
+        depth: 300,
+      },
+      {
+        id: 15,
+        filename: "quiet_beach.jpg",
+        depth: 300,
+      },
+    ],
+    pages: {
+      table: 4,
+      contents: 55,
+    },
+  },
+  {
+    id: 3,
+    name: "baz",
+    photos: [
+      {
+        id: 13,
+        filename: "sunny_beach.jpg",
+        depth: 2000,
+      },
+      {
+        id: 14,
+        filename: "mountain_view.jpg",
+        depth: 4000,
+      },
+    ],
+    pages: {
+      table: 8,
+      contents: 90,
+    },
+  },
+];
+let albumsJson = JSON.stringify(albumsPlain);
 
 class Pages {
   table = Exposed.number();
@@ -52,5 +97,5 @@ class Album {
   pages = Exposed.struct(Pages);
 }
 
-let album = plainToInstance(Album, JSON.parse(albumJson));
-console.log(album);
+let albums = plainsToInstances(Album, JSON.parse(albumsJson));
+console.log(albums);
